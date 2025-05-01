fa92dd2cc22778f80f62298bcac623c0e256ef1f
vuongquangminh120802@gmail.com <vuongquangminh120802@gmail.com>
2025-05-01 13:55:47 +0700

    update name

--- src/app/controllers/ChatController.js
+++ src/app/controllers/ChatController.js
@@ -1,18 +1,18 @@
-const Flow = require("../models/Chat");
+const Chat = require("../models/Chat");
 
-class FlowController {
+class ChatController {
   //GET: account
   async get(req, res, next) {
     console.log(req.query._id);
-    const query = await Flow.findOne({ _id: req.query._id });
+    const query = await Chat.findOne({ _id: req.query._id });
     res.json(query);
   }
   async post(req, res, next) {
-    const data = await Flow.create(req.body);
+    const data = await Chat.create(req.body);
     if (data) {
-      res.json({ data, message: "Bạn đã lưu dữ liệu flow thành công" });
+      res.json({ data, message: "Bạn đã lưu dữ liệu Chat thành công" });
     }
   }
 }
 
-module.exports = new FlowController();
+module.exports = new ChatController();
