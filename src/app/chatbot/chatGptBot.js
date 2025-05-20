5025a26d6c1f9d01d34ce0dc94eb8653d925e321
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-05-20 10:47:27 +0700

    export model

--- src/app/chatbot/chatGptBot.js
+++ src/app/chatbot/chatGptBot.js
@@ -1,20 +1,27 @@
 const { HumanMessage, AIMessage } = require("@langchain/core/messages");
 const { model } = require("../utils");
+const { getRedis } = require("../redis");
 
 let historyMessages = [];
 
 const chatgpt = async ({ content }) => {
   // Tạo mảng messages: lịch sử + message mới
-  const messages = [...historyMessages, new HumanMessage(content)];
-  const response = await model.invoke(messages);
 
-  // Cập nhật lại history
-  historyMessages.push(
-    new HumanMessage(content),
-    new AIMessage(response.content)
-  );
+  const getCache = await getRedis(content);
+  if (getCache) {
+    return getCache;
+  } else {
+    const messages = [...historyMessages, new HumanMessage(content)];
+    const response = await model.invoke(messages);
 
-  return response.content;
+    // Cập nhật lại history
+    historyMessages.push(
+      new HumanMessage(content),
+      new AIMessage(response.content)
+    );
+
+    return response.content;
+  }
 };
 
 module.exports = { chatgpt };
