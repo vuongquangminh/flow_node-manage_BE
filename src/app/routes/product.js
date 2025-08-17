224dac71d2e86af8d6d8dc23ad34ef8280f5673e
vuongquangminh120802@gmail.com <vuongquangminh120802@gmail.com>
2025-08-17 13:23:06 +0700

    chore : add middleware

--- src/app/routes/product.js
+++ src/app/routes/product.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const ProductController = require("../controllers/ProductController");
+const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/products", ProductController.get);
 router.get("/products/search", ProductController.search);
 router.get("/products/:id", ProductController.show);
 router.post("/product", ProductController.post);
-router.get("/admin/products", ProductController.getAll);
+router.get("/admin/products", authMiddleware, ProductController.getAll);
 
 module.exports = router;
